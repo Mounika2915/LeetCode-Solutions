53ec061a79c50b6cc0b7ff566421d8d8cd765f61
Mounika <155205651+Mounika2915@users.noreply.github.com>
2025-11-15 17:05:42 +0530

    Time: 97 ms (25.69%), Space: 100 MB (9.01%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.java
+++ 0883-car-fleet/0883-car-fleet.java
@@ -7,16 +7,22 @@ class Solution {
             res[i][1] =(double)(target - position[i])/speed[i] ;
         }
         Arrays.sort(res , (a ,b)-> Double.compare(b[0] , a[0])) ;
-        // Stack<Double> stk = new Stack<>();
-        // stk.push(res[0][1]);
-        double prevTime = 0;
-        int count = 0 ;
-        for(int i = 0 ; i < n ;i++){
-            if(prevTime < res[i][1]){
-                prevTime = res[i][1] ;
-                count++;
+        Stack<Double> stk = new Stack<>();
+        stk.push(res[0][1]);
+        for(int i = 1 ; i < n ;i++){
+            if(stk.peek() < res[i][1]){
+                stk.push(res[i][1]);
             }
         }
-        return count ;
+        return stk.size();
+        // double prevTime = 0;
+        // int count = 0 ;
+        // for(int i = 0 ; i < n ;i++){
+        //     if(prevTime < res[i][1]){
+        //         prevTime = res[i][1] ;
+        //         count++;
+        //     }
+        // }
+        // return count ;
     }
 }
